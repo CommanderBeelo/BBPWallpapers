891acba1fca23752030270cc9a9924d1820e367c
CommanderBeelo <xxxbilliamxxx@gmail.com>
2025-03-05 10:56:56 -0500

    Updated
    Fixed some small errors, added some new examples.

--- BBPWallpapers/config.cpp
+++ BBPWallpapers/config.cpp
@@ -40,19 +40,54 @@ class CfgVehicles
 {
 	class BBP_WallPaper;
 	class BBP_WallPaper_Mural;
-	class Beelo_Wallpaper: BBP_WallPaper
+	class Beelo_BBP_Wallpaper_Example: BBP_WallPaper
 	{
+		scope=2;
+		displayName="The WallPaper Example";
+		descriptionShort="super Sweet Example wallpaper";
 		hiddenSelectionsTextures[]=
 		{
-			"BBPWallpapers\wallpapers\wallpaper_example.paa"
+			"BBPWallpapers\wallpapers\WPExample.paa"
 		};
 	};
-	
-	class Beelo_Wallpaper_Mural: BBP_WallPaper_Mural
+	class Beelo_BBP_Wallpaper_Scamtopia: BBP_WallPaper
 	{
+		scope=2;
+		displayName="Scamtopia Corporation";
+		descriptionShort="Don't buy mods from Genesis";
 		hiddenSelectionsTextures[]=
 		{
-			"BBPWallpapers\wallpapers\wallpaper_mural_example_1.paa"
+			"BBPWallpapers\wallpapers\WPScamtopia.paa"
+		};
+	};
+	class Beelo_BBP_Mural_Example: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="The Mural Example";
+		descriptionShort="super Sweet Example";
+		hiddenSelectionsTextures[]=
+		{
+			"BBPWallpapers\wallpapers\MExample.paa"
+		};
+	};
+	class Beelo_Wallpaper_Mural_RedBricks: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Red Brick Mural";
+		descriptionShort="Red Brick Mural";
+		hiddenSelectionsTextures[]=
+		{
+			"BBPWallpapers\wallpapers\MRedBricks.paa"
+		};
+	};
+	class Beelo_Wallpaper_Mural_Scamtopia: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Fuck Genesis";
+		descriptionShort="Dont Buy Shit from Scamtopia, its bad for your bank account.";
+		hiddenSelectionsTextures[]=
+		{
+			"BBPWallpapers\wallpapers\MScamtopia.paa"
 		};
 	};
 };
